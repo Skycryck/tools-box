69acf01d842eeea754bd8adce4e01649575764d2
Jules Vanhulst <vanhulst79@gmail.com>
2018-03-07 16:38:42 +0100

    Corrections
    Suppression de deux div inutiles dans index.html. Correction du script des images de l'index et correction d'une faute de frappe dans le menu

--- js/image-index.js
+++ js/image-index.js
@@ -4,22 +4,28 @@ var filtre2 = document.getElementById("filtre2");
 var img1 = document.getElementById("img1");
 var img2 = document.getElementById("img2");
 
-function hoverFiltre1() {
-    filtre1.classList.remove("filtre-hidden");
-    img1.classList.add("image-hover");
+function hoverFiltre(i) { 
+    switch(i) { //i est le numero de l'image qui est en train d'etre survolee
+        case 1: //Image n°1
+            filtre1.classList.remove("filtre-hidden"); //Fait apparaitre la div qui sert de legend a l'image
+            img1.classList.add("image-hover"); //Zoom l'image
+            break;
+        case 2: //Image n°2
+            filtre2.classList.remove("filtre-hidden");
+            img2.classList.add("image-hover");
+            break;
+    }
 }
 
-function outFiltre1() {
-    filtre1.classList.add("filtre-hidden");
-    img1.classList.remove("image-hover");
-}
-
-function hoverFiltre2() {
-    filtre2.classList.remove("filtre-hidden");
-    img2.classList.add("image-hover");
-}
-
-function outFiltre2() {
-    filtre2.classList.add("filtre-hidden");
-    img2.classList.remove("image-hover");
+function outFiltre(i) {
+    switch(i) { //i est le numero de l'image qui est en train d'etre survolee
+    case 1: //Image n°1
+        filtre1.classList.add("filtre-hidden");
+        img1.classList.remove("image-hover");
+        break;
+    case 2: //Image n°2
+        filtre2.classList.add("filtre-hidden");
+        img2.classList.remove("image-hover");
+        break;
+    }
 }
