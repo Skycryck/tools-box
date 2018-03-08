02243f578e1cbb2e7bd42ee9fc40f424f0c9c1cf
Jules Vanhulst <vanhulst79@gmail.com>
2018-03-08 06:24:49 +0100

    Amelioration fonction recherche

--- js/recherche.js
+++ js/recherche.js
@@ -2,69 +2,84 @@
 var elements = [
     {
         label: "ACCUEIL",
-        lien: "../index.html"
+        lien: "../index.html",
+        rank: -1
     },
     {
         label: "CALCULETTE",
-        lien: "./calculette.html"
+        lien: "./calculette.html",
+        rank: -1
     },
     {
         label: "CONVERTISSEUR",
-        lien: "./convertisseur.html"
+        lien: "./convertisseur.html",
+        rank: -1
     },
     {
         label: "ASCII",
-        lien: "./ascii.html" 
+        lien: "./ascii.html",
+        rank: -1
     },
     {
         label: "CARTE",
-        lien: "./js.html" 
+        lien: "./js.html",
+        rank: -1
     },
     {
         label: "CONTACT",
-        lien: "./contact.html"
+        lien: "./contact.html",
+        rank: -1
     }
 ];
 
-var resultats = document.createElement("ul");
-resultats.id = "res";
-var recherche = document.getElementById("search");
+var res = document.createElement("ul");
+document.querySelector("article").appendChild(res);
+res.id = "res";
+
 
 function rechercher() {
+    removeRes();
     var recherche = document.getElementById("search").value.toUpperCase();
-    creationListe(elements);
+    //addList(rankList(elements, recherche));
+    setRankList(recherche);
 }
 
-function creationListe(liste) {
-    //document.parentNode.removeChild(resultats);
+//Ajoute la liste des resultats de la recherche a la page
+function addList(liste) {
+    var res = document.getElementById("res");
     for(var i = 0; i < liste.length; i++) {
         var li = document.createElement("li");
         var a = document.createElement("a");
         a.textContent = liste[i].label;
         a.href = liste[i].lien;
         li.appendChild(a);
-        resultats.appendChild(li);
+        res.appendChild(li);
     }
-    document.querySelector("article").appendChild(resultats);
+    document.querySelector("article").appendChild(res);
 }
 
-function rankListe(liste, recherche) {
-    var rankElements;
-    var found = -1;
-    for(var i = 0; i < liste.length; i++) {
-        if(recherche.includes(liste[i].label)) {
-            rankElements.push(liste[i]);
-            found = i;
+//Prend en parametre une liste et une recherche et retourne la liste ranger en fonction de la recherche
+function setRankList(recherche) {
+    var found = []
+    for(var i = 0; i < elements.length; i++) {
+        if(recherche.includes(elements[i].label)) { //Si la recherche contient un mot egale a un des labels place l'element du label en 1er
+            elements[i].rank = 0;
+            found.push(i);
         }
     }
     
-    for(var i = 0; i < liste.length; i++) {
-        if(i != found)
-            rankElements.push(liste[i]);
+    for(var j = 0; j < elements.length; j++) {
+        if(!found.includes(j-1)) {
+            elements[j].rank = j;
+        }
     }
-    return rankElements;
+    
+    for(var k = 0; k < elements.length; k++) {
+        console.log(elements[k].label + " " + elements[k].rank);
+    }
 }
 
+//Retourne le nombre de caracteres en commun entre motA et motB
 function nbCharComun(motA, motB) {
     var cpt = 0;
     var dejaTester = "";
@@ -77,3 +92,14 @@ function nbCharComun(motA, motB) {
     }
     return cpt;
 }
+
+//Efface les resultats de la recherche precedente
+function removeRes() {
+    var res = document.getElementById("res");
+    if(res.hasChildNodes()) {
+        while(res.firstChild) { //Retire le 1er enfant de res, tant que res contient un 1er enfant
+            res.removeChild(res.firstChild);
+        }
+    }
+}
+
