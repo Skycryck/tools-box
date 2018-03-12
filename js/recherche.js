75753b01ca554d61180bd648955e2c7f9a4e9c99
Skycryck <vanhulst79@gmail.com>
2018-03-12 13:42:21 +0100

    La recherche fonctionne maintenant à partir de tous les pages.

--- js/recherche.js
+++ js/recherche.js
@@ -1,52 +1,79 @@
 
-var elements = [
+var liens = [
     {
         label: "ACCUEIL",
         lien: "../index.html",
-        rank: -1
     },
     {
         label: "CALCULETTE",
         lien: "./calculette.html",
-        rank: -1
     },
     {
         label: "CONVERTISSEUR",
         lien: "./convertisseur.html",
-        rank: -1
     },
     {
         label: "ASCII",
         lien: "./ascii.html",
-        rank: -1
     },
     {
         label: "CARTE",
         lien: "./js.html",
-        rank: -1
     },
     {
         label: "CONTACT",
         lien: "./contact.html",
-        rank: -1
-    }
+    },
 ];
 
-var res = document.createElement("ul");
-document.querySelector("article").appendChild(res);
-res.id = "res";
-
+window.onload = function() {
+    var pos = document.location.href;
+    var recherche;
+    if(pos.includes("?")) {
+        recherche = pos.substring(pos.lastIndexOf("?")+1, pos.length);
+        console.log(recherche);
+        addList(sortList(liens, recherche));
+    }
+}
 
 function rechercher() {
-    removeRes();
     var recherche = document.getElementById("search").value.toUpperCase();
-    //addList(rankList(elements, recherche));
-    setRankList(recherche);
+    var pos = document.location.href;
+    if(!pos.includes("recherche.html")) {
+        redirection(recherche);
+    }
+    else {
+        removeRes();
+        addList(sortList(liens, recherche));
+    }
+}
+
+function redirection(recherche) {
+    var pos = document.location.href;
+    if(pos.includes("index.html"))
+        document.location.href = pos.substring(0,pos.lastIndexOf("/")) + "/pages/recherche.html?" + recherche;
+    else
+        document.location.href = pos.substring(0,pos.lastIndexOf("/")) + "/recherche.html?" + recherche;
+}
+
+//Prend une liste en parametre et retourne cette meme liste ranger en fonction des indices "rank" de cette liste
+//Trie par selection
+function sortList(list, recherche) {
+    for(var i = 0; i < list.length; i++) {
+        for(var j = i + 1; j < list.length; j++) {
+            if(getScore(list[i].label , recherche) < getScore(list[j].label, recherche)) {
+                var temp = list[j];
+                list[j] = list[i];
+                list[i] = temp;
+            }
+        }
+    }
+    return list;
 }
 
 //Ajoute la liste des resultats de la recherche a la page
 function addList(liste) {
-    var res = document.getElementById("res");
+    var res = document.createElement("ul");
     for(var i = 0; i < liste.length; i++) {
         var li = document.createElement("li");
         var a = document.createElement("a");
@@ -58,48 +85,36 @@ function addList(liste) {
     document.querySelector("article").appendChild(res);
 }
 
-//Prend en parametre une liste et une recherche et retourne la liste ranger en fonction de la recherche
-function setRankList(recherche) {
-    var found = []
-    for(var i = 0; i < elements.length; i++) {
-        if(recherche.includes(elements[i].label)) { //Si la recherche contient un mot egale a un des labels place l'element du label en 1er
-            elements[i].rank = 0;
-            found.push(i);
+//Efface les resultats de la recherche precedente
+function removeRes() {
+    var art = document.querySelector("article");
+    if(art.hasChildNodes()) {
+        while(art.firstChild) { //Retire le 1er enfant de res, tant que res contient un 1er enfant
+            art.removeChild(art.firstChild);
         }
     }
-    
-    for(var j = 0; j < elements.length; j++) {
-        if(!found.includes(j-1)) {
-            elements[j].rank = j;
+}
+
+//Prend deux mot en parametre et retourne un score plus le score est eleve plus les mots sont similaires
+function getScore(mot, recherche) {
+    var score = 0;
+    if(mot == recherche)
+        score += 100;
+    for(var i = 0; i < 20; i++) {
+        switch (nbCharComun(mot, recherche)) {
+            case i: score += i;
         }
     }
-    
-    for(var k = 0; k < elements.length; k++) {
-        console.log(elements[k].label + " " + elements[k].rank);
-    }
+    return score;
 }
 
 //Retourne le nombre de caracteres en commun entre motA et motB
 function nbCharComun(motA, motB) {
     var cpt = 0;
-    var dejaTester = "";
     for(var i = 0; i < motA.length; i++) {
-        var charTester = motA.charAt(i);
-        if(motB.includes(charTester) && (!dejaTester.includes(charTester))) {
+        var charTester = motA[i];
+        if(motB.includes(charTester))
             cpt++;
-            dejaTester = dejaTester + charTester;
-        }
     }
     return cpt;
 }
-
-//Efface les resultats de la recherche precedente
-function removeRes() {
-    var res = document.getElementById("res");
-    if(res.hasChildNodes()) {
-        while(res.firstChild) { //Retire le 1er enfant de res, tant que res contient un 1er enfant
-            res.removeChild(res.firstChild);
-        }
-    }
-}
-
